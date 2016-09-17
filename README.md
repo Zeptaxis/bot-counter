58e11e8307536cbd784c2f029890b6a56bce0c56
Zeptaxis <minercrafter1312 @gmail.com>
2016-09-17 17:29:31 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # bot-counter
 A Discord bot that can count things.
+
+#### Installation
+ - Download [Node.js][Node.js] **Version 6.0.0 or more**
+ - Open a command prompt and type :  
+    - `npm install --save discord.js --production --no-optional`
+    - `npm install properties-reader`
+ - Edit the `settings.properties` file and add your Discord API token. While you are at it, you can add your ID in the file so the bot know you are its owner. If you don't know how to get your ID, check next section.
+ - You can get your Discord API token [here][Discord API]. You need to create a new application, then create a bot user, then your token will be available in the `APP BOT USER` section.
+ - To invite your bot to your server, copy the following link and replace the `12345678` with the `Client ID` you can find on your application's page. `https://discordapp.com/oauth2/authorize?client_id=12345678&scope=bot&permissions=66186303`
+ - To run the bot, just open a command prompt in the bot folder and type `node bot-counter.js`
+ - To stop the bot, press `Ctrl + C`
+
+###### How to get your ID
+ - Run the bot and add it to your server
+ - Type `!uid` in a channel that the bot can access
+ - Copy your ID in the `settings.properties` file next to `ownerID = `
+ - Restart the bot for changes to work.
+
+#### Usage
+You can create counters with the `!addcounter name` command.  
+You can then show the counter with `!name` and interact with it with `!name +` or `!name -`.
+
+Full documentation will come soon !
+
+
+ [Node.js] : https://nodejs.org/
+ [Discord API] : https://discordapp.com/developers/applications/me
---
+++ bot-counter.js
@@ -0,0 +1,226 @@
+var PropertiesReader = require('properties-reader');
+var properties;
+try {
+	properties = PropertiesReader('settings.properties')
+} catch(err) {
+	console.log("You need to create a settings.properties file in the application folder. See README for more informations.");
+	process.exit(1);
+};
+
+
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = properties.get('token');
+if(!token) {
+	console.log("settings.properties file is missing a 'token = <your token>' line. Check README if you don't know how to get your token.");
+	process.exit(1);
+};
+
+var ownerID = properties.get('ownerID');
+
+var fs = require("fs");
+
+var inputFilter = /^[A-Za-z0-9]+$/;
+
+var counters;
+try {
+	counters = require('./counters.json');
+} catch (err) {
+	console.log(err);
+	counters = {};
+}
+
+bot.on('ready', () => {
+	console.log(' -- LOADED -- ');
+});
+
+bot.on('message', message => {
+	/*message.mentions.users.forEach(function (value, key, mapObj) {
+	console.log(value.id);
+	});*/
+
+	if (message.content.startsWith('!') && message.content.length > 1) {
+		var content = message.content.split(" ");
+
+		if (message.content.startsWith('!addcounter') || message.content.startsWith('!ac')) {
+			if (content.length == 2) {
+				var state = addCounter(message.author.id, content[1]);
+				if (state == 1) {
+					message.channel.sendMessage('The counter has been correctly added.\r\nYou can use it with !' + content[1] + ' [ + | - ].');
+				} else if (state == 2) {
+					message.channel.sendMessage('A counter with this name already exists, please choose another one.');
+				} else if (state == 3) {
+					message.channel.sendMessage('Your counter name contains illegal characters. Please match /^[A-Za-z0-9]+$/.');
+				}
+			}
+		} else if (message.content.startsWith('!delcounter') || message.content.startsWith('!dc')) {
+			if (content.length == 2) {
+				var state = delCounter(message.author.id, content[1]);
+				if (state == 1) {
+					message.channel.sendMessage('The counter has been correctly deleted.');
+				} else if (state == 2) {
+					message.channel.sendMessage('There is no counter with this name.');
+				} else if (state == 3) {
+					message.channel.sendMessage('You are not the owner of this counter.');
+				}
+			}
+		} else if (message.content == "!log") {
+			console.log(counters);
+		} else if (message.content == "!cleardb") {
+			if (message.author.id == ownerID) {
+				counters = {};
+				saveToDisk();
+			} else {
+				message.channel.sendMessage('Sorry, only the owner can do this.');
+			}
+		} else if(message.content == "!uid") {
+			message.channel.sendMessage('Your UID is : ' + message.author.id)
+		} else {
+			var counterName = content[0].substring(1);
+			if (counters[counterName]) {
+				if (content.length == 1) {
+					message.channel.sendMessage(getTextView(counterName));
+				} else {
+					if (content[1] == '+') {
+						incrementValue(counterName);
+						message.channel.sendMessage(getTextPlus(counterName));
+					} else if (content[1] == '-') {
+						decrementValue(counterName);
+						message.channel.sendMessage(getTextMinus(counterName));
+					} else if (content[1] == 'reset') {
+						resetValue(counterName);
+						message.channel.sendMessage(getTextReset(counterName));
+					} else if (content[1] == 'value') {
+						if (content[2]) {
+							setValue(counterName, content[2]);
+							message.channel.sendMessage(getTextValue(counterName));
+						}
+					} else if (content[1] == 'edit') {
+						if (counters[counterName][content[2]]) {
+							var newValue = message.content.substr(message.content.indexOf(content[2]) + content[2].length + 1);
+							setCounterText(counterName, content[2], newValue);
+						}
+					} else if (content[1] == 'show') {
+						if (counters[counterName][content[2]]) {
+							message.channel.sendMessage(content[2] + ' : ' + counters[counterName][content[2]]);
+						}
+					}
+					saveToDisk();
+				}
+			}
+		}
+	}
+
+});
+
+bot.login(token);
+
+function addCounter(id, title) {
+	if (inputFilter.test(title) && title != "addcounter" && title != "delcounter") {
+		if (counters[title]) {
+			return 2;
+		} else {
+			counters[title] = {
+				owner : id,
+				value : 0,
+				step : 1,
+				name : title,
+				textView : 'Value of %name% : %value%',
+				textPlus : 'The value of %name% has been incremented. New value : %value%.',
+				textMinus : 'The value of %name% has been decremented. New value : %value%.',
+				textReset : 'The value of %name% has been reset to %value%.',
+				textValue : 'The value of %name% has been set to %value%.'
+			};
+			saveToDisk();
+			return 1;
+		}
+	} else {
+		return 3;
+	}
+}
+
+function getTextView(title) {
+	return counters[title].textView.replace('%name%', counters[title].name).replace('%value%', counters[title].value);
+}
+
+function getTextPlus(title) {
+	return counters[title].textPlus.replace('%name%', counters[title].name).replace('%value%', counters[title].value);
+}
+
+function getTextMinus(title) {
+	return counters[title].textMinus.replace('%name%', counters[title].name).replace('%value%', counters[title].value);
+}
+
+function getTextReset(title) {
+	return counters[title].textReset.replace('%name%', counters[title].name).replace('%value%', counters[title].value);
+}
+
+function getTextValue(title) {
+	return counters[title].textValue.replace('%name%', counters[title].name).replace('%value%', counters[title].value);
+}
+
+function setCounterText(title, textToChange, newText) {
+	counters[title][textToChange] = newText;
+}
+
+function incrementValue(title) {
+	setValue(title, getValue(title) + getStep(title));
+}
+
+function decrementValue(title) {
+	setValue(title, getValue(title) - getStep(title));
+}
+
+function resetValue(title) {
+	setValue(title, getValue(title) + 1);
+}
+
+// set the value of the specified counter. The value is sanitized to be always an integer.
+function setValue(title, newValue) {
+	var val = parseInt(newValue);
+	if (!isNaN(val)) {
+		counters[title].value = val;
+	}
+
+}
+
+function getValue(title) {
+	// since the value can be invalide due to the edit command, we check that it is an integer and reset it when needed
+	var val = parseInt(counters[title].value);
+	if (isNaN(val)) {
+		counters[title].value = val = 0;
+	}
+	return val;
+}
+
+function getStep(title) {
+	// since the value of step can be invalide due to the edit command, we check that it is an integer and reset it when needed
+	var val = parseInt(counters[title].step);
+	if (isNaN(val)) {
+		counters[title].step = val = 1;
+	}
+	return val;
+}
+
+function delCounter(id, title) {
+	if (inputFilter.test(title)) {
+		if (counters[title]) {
+			if (id != counters[title].owner) {
+				return 3;
+			} else {
+				delete counters[title];
+				return 1;
+			}
+		} else {
+			return 2;
+		}
+
+	} else {
+		return 2;
+	}
+}
+
+function saveToDisk() {
+	fs.writeFile('counters.json', JSON.stringify(counters), "utf8");
+}
